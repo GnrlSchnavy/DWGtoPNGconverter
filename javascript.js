e734b9d72b9a947a105351ce088222dde3796624
GnrlSchnavy <yvanstemmerik@gmail.com>
2017-11-09 12:35:57 +0100

    Combined everything now also tries to do exterior walls coordinates

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function main(){ 
+ function main(){ 
 
 	//Start imports
 	include("scripts/File/Print/Print.js");
@@ -10,24 +10,22 @@ function main(){
 	doc.setUnit(RS.Millimeter);
 
 	//Do import, fix layers and remove text
-	var textFile = readTextFile(path+'layers.txt'); 
-	importFiles(doc, di);
-	removeText(doc,di);
-	turnOnLayers(doc,di,textFile);
-	var scene = new RGraphicsSceneQt(di);
+	var textFile = readTextFile(path+'layers.txt');   //Read layers from text file
+	importFiles(doc, di);							  //Import the dwg file passed through the bash script
+	removeText(doc,di);								  //Remove all the text and text-based elements from the drawing
+	turnOnLayers(doc,di,textFile);					  //Turn on the selected layers
+	var scene = new RGraphicsSceneQt(di);			  
 	var view = new RGraphicsViewImage();
-	setPageVariables(doc,di,view,scene);
+	setPageVariables(doc,di,view,scene);			  //set offsets, page width and height, grayscale, hairlines etc.
 
 	//Start export and creation of maps
-	var exporter = new Print(undefined, doc, view);
+	var exporter = new Print(undefined, doc, view);	  
 	filename = inputfile.substring(inputfile.lastIndexOf("/")+1,inputfile.length-4);
 	path2 = path+"Helperfiles/"+filename+".pdf";
-	exporter.print(path2);
+	exporter.print(path2);							  //Export to PDF
 	print("\n\nMESSAGE1:file exported to " + path2 + "\n\n");
-	di.exportFile(path+"Helperfiles/"+filename+".dxf","DFX 2000");
+	di.exportFile(path+"Helperfiles/"+filename+".dxf","DFX 2000");	//Export to dxf
 
-	//explode everthing
-	//check for layers with #### export those to seperate map
 }
 
 function prepareDirectory(path){
---
+++ lines.py
@@ -0,0 +1,185 @@
+import csv
+import sys
+import json
+import os
+import math
+from PIL import Image, ImageDraw
+#todo
+# put this in the full program, why are some pixels missing?!!?!
+#find a way to fix outer
+
+pixelList = []
+scannedPixels = []
+linesList = []
+
+def main(argv):
+    filename = sys.argv[1]
+    if(not os.path.isdir("Coordinatefiles")):
+        os.makedirs("Coordinatefiles")
+    path = "Coordinatefiles/"+"final"+filename
+    f= open(path,"w+")
+
+    csvPath = "Coordinatefiles/"+filename
+    with open(csvPath, newline='') as csvfile:
+        file = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+        for row in file:
+            xCoordinate=row[0].split(',')[0]
+            yCoordinate=row[0].split(',')[1]
+            coordinate=[int(xCoordinate),int(yCoordinate)]
+            pixelList.append(coordinate)
+
+    for pixel in pixelList:
+
+        if(not checkAlreadyScanned(pixel)):
+            if(hasRightPixel(pixel)):
+                line = [0, 0]
+                line[0]=[pixel[0],pixel[1]]
+                temp=pixel
+                if (isExistingPixel(pixel) and not moreThanOneSurroundingPixel(pixel)):
+                    scannedPixels.append(pixel)
+                    pixelList.remove(pixel)
+                while(hasRightPixel(temp)):
+                    if(isExistingPixel(temp) and not moreThanOneSurroundingPixel(temp)):
+                        scannedPixels.append(temp)
+                        pixelList.remove(temp)
+                    temp = [temp[0] + 1, temp[1]]
+                    line[1]=temp
+                linesList.append(line)
+
+            if(hasLowerPixel(pixel)):
+                line = [0, 0]
+                line[0]=[pixel[0],pixel[1]]
+                temp=pixel
+                if (isExistingPixel(pixel) and not moreThanOneSurroundingPixel(pixel)):
+                    scannedPixels.append(pixel)
+                    pixelList.remove(pixel)
+                while(hasLowerPixel(temp)):
+                    if (isExistingPixel(temp) and not moreThanOneSurroundingPixel(temp)):
+                        scannedPixels.append(temp)
+                        pixelList.remove(temp)
+                    temp=[temp[0],temp[1]+1]
+                    line[1]=temp
+                linesList.append(line)
+
+            if(hasLowerLeftPixel(pixel)):
+                line = [0, 0]
+                line[0] = [pixel[0], pixel[1]]
+                temp = pixel
+                if (isExistingPixel(pixel) and not moreThanOneSurroundingPixel(pixel)):
+                    scannedPixels.append(pixel)
+                    pixelList.remove(pixel)
+                while (hasLowerLeftPixel(temp)):
+                    if (isExistingPixel(temp) and not moreThanOneSurroundingPixel(temp)):
+                        scannedPixels.append(temp)
+                        pixelList.remove(temp)
+                    temp = [temp[0]-1, temp[1] + 1]
+                    line[1] = temp
+                linesList.append(line)
+
+            if(hasLowerRightPixel(pixel)):
+                line = [0, 0]
+                line[0] = [pixel[0], pixel[1]]
+                temp = pixel
+                if (isExistingPixel(pixel) and not moreThanOneSurroundingPixel(pixel)):
+                    scannedPixels.append(pixel)
+                    pixelList.remove(pixel)
+                while (hasLowerRightPixel(temp)):
+                    if (isExistingPixel(temp) and not moreThanOneSurroundingPixel(temp)):
+                        scannedPixels.append(temp)
+                        pixelList.remove(temp)
+                    temp = [temp[0]+1, temp[1] + 1]
+                    line[1] = temp
+                linesList.append(line)
+
+    draw(pixelList,linesList,argv)
+
+
+    for x in linesList:
+        f.write(json.dumps(x))
+    for x in pixelList:
+        f.write(json.dumps(x))
+
+def toText(list):
+    str1 = ''.join(''.join(list))
+    return str1
+
+def moreThanOneSurroundingPixel(pixel):
+    surroundingpixels=0
+    # print(pixel)
+    if(isExistingPixel([pixel[0]+1,pixel[1]])):
+        surroundingpixels+=1
+    if(isExistingPixel([pixel[0],pixel[1]+1])):
+        surroundingpixels+=1
+    if(isExistingPixel([pixel[0]+1,pixel[1]-1])):
+        surroundingpixels+=1
+    if(isExistingPixel([pixel[0]+1,pixel[1]+1])):
+        surroundingpixels+=1
+    if(isExistingPixel([pixel[0]-1,pixel[1]+1])):
+        surroundingpixels+=1
+    # print(surroundingpixels)
+    if(surroundingpixels>2):
+        # print(pixel, "has surrounding")
+        return True
+    else:
+        return False
+
+def hasLowerPixel(pixel):
+    lowerPixel = [pixel[0],pixel[1]+1]
+    if(isExistingPixel(lowerPixel)):
+        return True
+    return False
+
+def hasRightPixel(pixel):
+    rightpixel = [pixel[0]+1,pixel[1]]
+    if(isExistingPixel(rightpixel)):
+        return True
+    return False
+
+def hasLowerLeftPixel(pixel):
+    lowerLeftPixel = [pixel[0]-1,pixel[1]+1]
+    if(isExistingPixel(lowerLeftPixel)):
+        return True
+    return False
+
+def hasLowerRightPixel(pixel):
+    lowerRightPixel = [pixel[0] + 1, pixel[1] + 1]
+    if (isExistingPixel(lowerRightPixel)):
+        return True
+    return False
+
+def isExistingPixel(toCheckPixel):
+    # print(toCheckPixel)
+    for pixel in pixelList:
+        if(pixel == toCheckPixel):
+            return True
+    return False
+
+
+
+def checkAlreadyScanned(pixel):
+    for scannedpixel in scannedPixels:
+        if(pixel == scannedpixel):
+            return True
+    return False
+
+def draw(pixels, lines,argv):
+    # width, sep, tail = argv[2].partition('.')
+    # height,sep, tail = argv[3].partition('.')
+    # canvas = (int(width), int(height))
+    canvas = (2046, 1300)
+    scale = 1
+    thumb = canvas[0] / scale, canvas[1] / scale
+    im = Image.new('RGBA', canvas, (255, 255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    for line in linesList:
+        draw.line((line[0][0],line[0][1],line[1][0],line[1][1]),fill="black")
+    for pixel in pixelList:
+        draw.point([pixel[0],pixel[1]],fill="black")
+    im.thumbnail(thumb)
+    pngfile , sep, tail = argv[1].partition('.')
+    im.save("Coordinatefiles/"+pngfile+'.png')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
